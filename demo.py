55095e29d18c359f772964665d58451d1b73a1ca
James Colestock <james@colestock.com>
2021-05-02 15:05:47 -0600

    demo changes to diagnose heroku crashing

--- demo.py
+++ demo.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 from src import server
 
 env = os.getenv("TORNADO_ENV", "production")
-if env == "development":
-    load_dotenv()  # take environment variables from .env
+print(env)
+load_dotenv()  # take environment variables from .env
+# if env == "development":
 
 if __name__ == '__main__':
     server().start()
